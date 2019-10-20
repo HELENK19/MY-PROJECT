a863ae5a91cdf552176bf4ea05d35dcd1425d569
WadeQ <derrickwadek@gmail.com>
2019-10-20 06:05:18 +0300

    Added animations to MainDishesDetailsActivity

--- app/src/main/java/com/team19/animation/ui/BounceInterpolater.java
+++ app/src/main/java/com/team19/animation/ui/BounceInterpolater.java
@@ -1,4 +1,19 @@
 package com.team19.animation.ui;
 
-public class BounceInterpolater {
+import android.view.animation.Interpolator;
+
+public class BounceInterpolater implements Interpolator {
+
+    private double mAmplitude = 1;
+    private double mFrequency = 10;
+
+    BounceInterpolater(double amplitude, double frequency) {
+        mAmplitude = amplitude;
+        mFrequency = frequency;
+    }
+
+    public float getInterpolation(float v) {
+        return (float) (-1 * Math.pow(Math.E, -v/ mAmplitude) *
+                Math.cos(mFrequency * v) + 1);
+    }
 }
