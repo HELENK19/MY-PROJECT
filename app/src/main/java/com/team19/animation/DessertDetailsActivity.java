3d122a2acd62281381efb9e0926602c282e4d9fb
Snilloc <32978824+Knightbot101@users.noreply.github.com>
2019-10-21 15:55:34 +0300

    Drinks Details Activity

---
+++ app/src/main/java/com/team19/animation/DessertDetailsActivity.java
@@ -0,0 +1,29 @@
+package com.team19.animation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class DessertDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dessert_detail);
+    }
+
+    public void sendRequestClicked(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(),"Request sent",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void addToCart(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(),"Added to cart",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void backButtonClicked(View view) {
+        //Going back to the main activity
+    }
+}
